8692ee147624c8653e4a3729a4c8ae80074d1d4f
Shawn Lian <admin@mail.xrlian.com>
2024-12-10 21:02:47 -0500

    WIP

--- test/run_all_tests.sh
+++ test/run_all_tests.sh
@@ -157,7 +157,7 @@ run_cproxy_cgroup_path_test() {
 
     # Run curl within the specified cgroup
     echo "Running curl within cgroup $CGROUP_PATH..."
-    sudo cgexec -g "unified:$CGROUP_NAME" curl -s -I https://www.google.com > /dev/null
+    sudo cgexec -g "*:$CGROUP_NAME" curl -s -I https://www.google.com > /dev/null
 
     if [ $? -eq 0 ]; then
         echo "cproxy --cgroup-path test: SUCCESS"
